514cf9530125ffe0137a65f8497f3702849fe8d8
Pavelas Morozovas <madstylecow@gmail.com>
2018-06-18 23:15:18 +0300

    Slight update

--- examples/equipe_timekeeping_client/src/App.js
+++ examples/equipe_timekeeping_client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './equipe.png';
 import Timekeeping from './Timekeeping.js';
 import './App.css';
 
@@ -8,10 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
-        <Timekeeping url="ws://192.168.1.76:21000"/>
+        <Timekeeping url="ws://127.0.0.1:21000"/>
       </div>
     );
   }
--- examples/equipe_timekeeping_client/src/Timekeeping.js
+++ examples/equipe_timekeeping_client/src/Timekeeping.js
@@ -5,145 +5,172 @@ import _ from 'lodash';
 
 class Timekeeping extends Component {
 
-  constructor(props) {
-    super(props);
+	// Class constructor
+	constructor(props) {
+		super(props);
 
-    this.onOpen = this.onOpen.bind(this);
-    this.onClose = this.onClose.bind(this);
-    this.updateTime = this.updateTime.bind(this);
-    this.start = this.start.bind(this);
-    this.stop = this.stop.bind(this);
-    this.fixedTo = this.fixedTo.bind(this);
-    this.output = this.output.bind(this);
-    this.onMessage = this.onMessage.bind(this);
+		this.onOpen = this.onOpen.bind(this);
+		this.onClose = this.onClose.bind(this);
+		this.updateTime = this.updateTime.bind(this);
+		this.start = this.start.bind(this);
+		this.stop = this.stop.bind(this);
+		this.fixedTo = this.fixedTo.bind(this);
+		this.output = this.output.bind(this);
+		this.onMessage = this.onMessage.bind(this);
 
-    this.state = {
-      connected: false,
-      time: null,
-      previousTime: null,
-      running: false,
-      countDown: false,
-      countDownDiff: null,
-      countDownValue: null,
-      waiting: false,
-      faults: null,
-      totalFaults: null,
-      fenceFaults: null,
-      timeFaults: null,
-      timeAdded: null,
-      id: null,
-      startNo: null,
-      rider: null,
-      horse: null,
-      round: 1,
-      phase: 1,
-      timeToBeatDiff: null,
-      timeToBeatTime: null,
-      timekeepingOutputId: null
-    };
-  }
+		this.state = {
+			connected: false,
+			time: null,
+			previousTime: null,
+			running: false,
+			countDown: false,
+			countDownDiff: null,
+			countDownValue: null,
+			waiting: false,
+			faults: null,
+			totalFaults: null,
+			fenceFaults: null,
+			timeFaults: null,
+			timeAdded: null,
+			id: null,
+			startNo: null,
+			rider: null,
+			horse: null,
+			round: 1,
+			phase: 1,
+			timeToBeatDiff: null,
+			timeToBeatTime: null,
+			timekeepingOutputId: null
+		};
+	}
 
-  componentDidMount(){
-    this.webSocket = new WebSocket(this.props.url);
-    this.webSocket.onopen = this.onOpen;
-    this.webSocket.onclose = this.onClose;
-    this.webSocket.onmessage = this.onMessage;
-  }
+	// On mount event
+	componentDidMount(){
+		this.webSocket = new WebSocket(this.props.url);
+		this.webSocket.onopen = this.onOpen;
+		this.webSocket.onclose = this.onClose;
+		this.webSocket.onmessage = this.onMessage;
+	}
 
-  componentWillUnmount(){
-    this.webSocket.close();
-    this.webSocket = null;
-  }
+	// On dismount event
+	componentWillUnmount(){
+		this.webSocket.close();
+		this.webSocket = null;
+	}
 
-  onOpen(){
-    this.setState({ connected: true });
-  }
+	// On WS opened event
+	onOpen(){
+		this.setState({ connected: true });
+	}
 
-  onClose(){
-    this.setState({ connected: false });
-  }
+	// On WS closed event
+	onClose(){
+		this.setState({ connected: false });
+	}
 
-  onMessage(event){
-    let data = JSON.parse(event.data);
-    if(data.type === 'output')
-      this.output(data.payload);
-  }
+	// On JSON message received event
+	onMessage(event){
+		let data = JSON.parse(event.data);
 
-  output(data){
-    console.log(data);
+		if(data.type === 'output')
+			this.output(data.payload);
+	}
 
-    // If eliminated hide the clock
-    data.isEliminated = !!ShowJumpingEvent[data.fenceFaults];
+	// Output function
+	output(data){
+		// If eliminated hide the clock
+		data.isEliminated = !!ShowJumpingEvent[data.fenceFaults];
 
-    // Normalize
-    data.faults = data.faults || 0;
-    if(!data.timeAdded)
-      data.timeAdded = null;
+		// Normalize
+		data.faults = data.faults || 0;
+		if(!data.timeAdded)
+			data.timeAdded = null;
 
-    if(data.countDownValue)
-      data.countDownValue = parseFloat(data.countDownValue, 10);
+		if(data.countDownValue){
+			data.countDownValue = parseFloat(data.countDownValue, 10);
+		}
 
-    if (data.countDown && !data.running)
-      this.start(Date.now() - (data.countDownValue * 1000));
-    else
-      if(data.running)
-        this.start(Date.now() - (parseFloat(data.time, 10) * 1000));
-      else {
-        if(this.state.countDown && !data.countDown)
-          data.countDown = true;
+		if (data.startNo) {
+			data.startNo += '.';
+		}
 
-        data.time = this.fixedTo(parseFloat(data.time, 10) || 0, 2);
-        data.countDownValue = this.fixedTo(parseFloat(data.countDownValue, 10), 2);
-        this.stop();
-      }
+		if (data.rank) {
+			data.rank += '.';
+		}
 
-    this.setState(data);
-  }
+		if (data.countDown && !data.running) {
+			this.start(Date.now() - (data.countDownValue * 1000));
+		}
+		else {
+			if(data.running)
+				this.start(Date.now() - (parseFloat(data.time, 10) * 1000));
+			else {
+				if(this.state.countDown && !data.countDown)
+					data.countDown = true;
 
-  fixedTo(number, digits) {
-    if(_.isFinite(number)){
-      var k = Math.pow(10, digits);
-      var value = Math.round(number * k) / k;
-      return value.toFixed(digits);
-    } else {
-      return null;
-    }
-  }
+				data.time = this.fixedTo(parseFloat(data.time, 10) || 0, 2);
+				data.countDownValue = this.fixedTo(parseFloat(data.countDownValue, 10), 2);
+				this.stop();
+			}
+		}
 
-  updateTime(){
-    var t = (Date.now() - this.startTick) / 1000;
-    if(this.state.countDown)
-      this.setState({ countDownValue: this.fixedTo(t, 1) });
-    else
-      if(this.state.running)
-        this.setState({ time: this.fixedTo(t, 1) });
-  }
+		this.setState(data);
+	}
 
-  start(tick) {
-    this.startTick = tick;
-    if(!this.timer)
-      this.timer = setInterval(this.updateTime, 100);
-  }
+	// Helper fixedTo
+	fixedTo(number, digits) {
+		if(_.isFinite(number)){
+			var k = Math.pow(10, digits);
+			var value = Math.round(number * k) / k;
+			return value.toFixed(digits);
+		} else {
+			return null;
+		}
+	}
 
-  stop() {
-    clearInterval(this.timer);
-    this.timer = null;
-  }
+	// Helper update time
+	updateTime(){
+		var t = (Date.now() - this.startTick) / 1000;
+		if(this.state.countDown)
+			this.setState({ countDownValue: this.fixedTo(t, 1) });
+		else
+			if(this.state.running)
+				this.setState({ time: this.fixedTo(t, 1) });
+	}
 
-  render() {
-    return (
-      <div>
-        <span>{this.state.connected ? 'connected' : 'disconnected'}</span>
-        <h1>Start no: {this.state.startNo} ({this.state.id})</h1>
-        <h1>Rider: {this.state.rider}</h1>
-        <h2>Horse: {this.state.horse}</h2>
-        <h3>Time: {this.state.time}</h3>
-        <h3>Faults: {this.state.faults}</h3>
-        <h4>countDown: {this.state.countDownValue}</h4>
-        <h4>Rank: {this.state.rank}</h4>
-      </div>
-    );
-  }
+	// Helper start
+	start(tick) {
+		this.startTick = tick;
+		if(!this.timer)
+			this.timer = setInterval(this.updateTime, 100);
+	}
+
+	// Helper stop
+	stop() {
+		clearInterval(this.timer);
+		this.timer = null;
+	}
+
+	// Render function
+	render() {
+		return (
+			<div>
+				<div className="App-dataContainer">
+					<span className="App-dataLine">
+						<span className={`App-id ${(!this.state.countDown && !this.state.running && this.state.rank !== '999.') ? 'red' : ''}`}>{ (!this.state.countDown && !this.state.running && this.state.rank !== '999.') ? this.state.rank : this.state.startNo }</span>
+						<span>{ this.state.rider }</span>
+					</span>
+					<span className="App-dataLine">
+						<span>{ this.state.horse }</span>
+					</span>
+				</div>
+				<div className="App-timeContainer">
+					<span>{ this.state.faults }</span>
+					<span>{ this.state.countDown ? this.state.countDownValue : this.state.time }</span>
+				</div>
+			</div>
+		);
+	}
 }
 
 Timekeeping.propTypes = { url: React.PropTypes.string.isRequired };
